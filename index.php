5a362393f313640193fe164541a880d6009bb999
yuhi_deve <yuhinnn@gmail.com>
2017-11-14 14:42:40 +0900

    fix margin

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>kamoがネギをしょってくるッ!!! PROFILE</title>
+    <title>kawaiimap撮影会</title>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="shortcut icon" href="kamonegi_favicon.png">
