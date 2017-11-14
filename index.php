be2d43ffa0ade26183f8d2f13e71dfbb31667ad2
yuhi_deve <yuhinnn@gmail.com>
2017-11-14 14:32:51 +0900

    fix

--- index.php
+++ index.php
@@ -8,7 +8,15 @@
     <link rel="shortcut icon" href="kamonegi_favicon.png">
 </head>
 <body>
-   <img class="content"src="lp.jpg" alt="">
-    
+
+<div class="relative">
+  <img class="lp"src="lp.jpg" alt="">
+  <a href="https://ws.formzu.net/fgen/S81114783/"><img class="btn-top"src="btn.png" alt=""></a>
+  <a href="https://ws.formzu.net/fgen/S81114783/"><img class="btn-bottom"src="btn.png" alt=""></a>
+  <a href="https://ws.formzu.net/fgen/S81114783/"><img class="twitter"src="twitter.png" alt=""></a>
+  <a href="mailto:omlts.abekobe@gmail.com"><img class="mail"src="mail.png" alt=""></a>
+</div>
+
 </body>
 </html>
+
